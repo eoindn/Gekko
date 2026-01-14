9baff7de89419983c2cb34fca7bb8e4b372776cd
josephulasii <jojoulasi7@gmail.com>
2026-01-14 16:47:24 +0000

    can now add files that have been created to a quarantine folder

--- detector/file_watcher.py
+++ detector/file_watcher.py
@@ -1,14 +1,30 @@
 from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import shutil
+import os
 
 
 class Handler(FileSystemEventHandler):
     
-    def on_created(self,event):
-        if event.is_directory:
+    def on_created(self, event):
+        if event.is_directory: 
             return
         print(f"File Created: {event.src_path}")
+        
+    
+        filename = Path(event.src_path).name
+        
+        
+        destination = Path("quarantine") / filename
+        
+        try:
+            shutil.copy2(event.src_path, destination)
+            print(f"Quarantined: {destination}")
+        except Exception as e:
+            print(f"Failed to quarantine: {e}")
+        
+        
     
     def on_deleted(self,event):
         if event.is_directory:
@@ -29,10 +45,13 @@ def main():
     observer.schedule(handler, path, recursive=True)
     observer.start()
     
-    while True:
-        cmd = input("> ")
-        if cmd == "q":
-            break
+    try:
+        while True:
+            pass  
+    except KeyboardInterrupt:
+        print("\n✓ Stopping...")
+    
+    
     observer.stop()
     observer.join()
 
