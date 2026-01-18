eec6ac6c665c35763118b9b52522fde63ea3f000
josephulasii <jojoulasi7@gmail.com>
2026-01-18 18:23:37 +0000

    hashes quarantined files using sha256

--- detector/file_watcher.py
+++ detector/file_watcher.py
@@ -2,25 +2,33 @@ from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import shutil
-import os
+import hashlib
 
+def calculate_hashes(filepath):
+        with open(filepath, 'rb') as f:
+            data = f.read()
+            result = hashlib.sha256(data).hexdigest()
+            print( "file hashed using SHA256:" + result)
+            return result
 
 class Handler(FileSystemEventHandler):
-    
+
+        
     def on_created(self, event):
         if event.is_directory: 
             return
         print(f"File Created: {event.src_path}")
-        
-    
         filename = Path(event.src_path).name
-        
-        
         destination = Path("quarantine") / filename
+
         
+
         try:
             shutil.copy2(event.src_path, destination)
             print(f"Quarantined: {destination}")
+            
+            hashed_file = calculate_hashes(destination)
+            
         except Exception as e:
             print(f"Failed to quarantine: {e}")
         
@@ -36,7 +44,7 @@ class Handler(FileSystemEventHandler):
             return
         print(f"File Modified: {event.src_path}")
 
-   
+
    
 def main():
     path = Path("testfolder")
@@ -50,6 +58,7 @@ def main():
             pass  
     except KeyboardInterrupt:
         print("\n✓ Stopping...")
+        
     
     
     observer.stop()
